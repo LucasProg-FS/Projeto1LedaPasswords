29e1e49c8c0a2067d8cd090b07e8deb160a3d036
Lucas Anacleto <lucasanacleto04@gmail.com>
2024-04-20 15:01:00 -0300

    Implementação do insertionSort para ordenação e criação dos aqruivos .csv para cada caso

--- src/Main.java
+++ src/Main.java
@@ -31,6 +31,7 @@ public class Main {
         System.out.println("\n\nCasos criados");
 
         SelectionSort selectionSort = new SelectionSort();
+        InsertionSort insertionSort = new InsertionSort();
 
         System.out.println("Inicializando ordenações...\n\n");
 
@@ -39,14 +40,30 @@ public class Main {
         selectionSort.length(mediumCaseByLength, "médio caso");
         selectionSort.length(worstCaseByLength, "pior caso");
 
+        System.out.println("\nInsertionSort P/ Tamanho da Senha:");
+        insertionSort.length(bestCaseByLength, "melhor caso");
+        insertionSort.length(mediumCaseByLength, "médio caso");
+        insertionSort.length(worstCaseByLength, "pior caso");
+
+
         System.out.println("\nSelectionSort P/ Mês:");
         selectionSort.month(bestCaseByMonth, "melhor caso");
         selectionSort.month(mediumCaseByMonth, "médio caso");
         selectionSort.month(worstCaseByMonth, "pior caso");
 
+        System.out.println("\nInsertionSort P/ Mês:");
+        insertionSort.month(bestCaseByMonth, "melhor caso");
+        insertionSort.month(mediumCaseByMonth, "médio caso");
+        insertionSort.month(worstCaseByMonth, "pior caso");
+
         System.out.println("\nSelectionSort P/ Data:");
         selectionSort.date(bestCaseByDate, "melhor caso");
         selectionSort.date(mediumCaseByDate, "médio caso");
         selectionSort.date(worstCaseByDate, "pior caso");
+
+        System.out.println("\nInsertionSort P/ Data:");
+        insertionSort.date(bestCaseByDate, "melhor caso");
+        insertionSort.date(mediumCaseByDate, "médio caso");
+        insertionSort.date(worstCaseByDate, "pior caso");
     }
 }
